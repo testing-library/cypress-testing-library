4ef82dfb81925a0954ec77c0756dc2116669f621
Kent C. Dodds <kent+github@doddsfamily.us>
2018-04-10 07:04:31 -0600

    Update commands.spec.js

--- cypress/integration/commands.spec.js
+++ cypress/integration/commands.spec.js
@@ -1,7 +1,9 @@
 describe('dom-testing-library commands', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
   it('getByPlaceholderText', () => {
     cy
-      .visit('http://localhost:13370')
       .getByPlaceholderText('Placeholder Text')
       .click()
       .type('Hello Placeholder')
@@ -9,14 +11,12 @@ describe('dom-testing-library commands', () => {
 
   it('getByText', () => {
     cy
-      .visit('http://localhost:13370')
       .getByText('Button Text')
       .click()
   })
 
   it('getByLabelText', () => {
     cy
-      .visit('http://localhost:13370')
       .getByLabelText('Label For Input Labelled By Id')
       .click()
       .type('Hello Input Labelled By Id')
@@ -24,14 +24,12 @@ describe('dom-testing-library commands', () => {
 
   it('getByAltText', () => {
     cy
-      .visit('http://localhost:13370')
       .getByAltText('Image Alt Text')
       .click()
   })
 
   it('getByTestId', () => {
     cy
-      .visit('http://localhost:13370')
       .getByTestId('image-with-random-alt-tag')
       .click()
   })
