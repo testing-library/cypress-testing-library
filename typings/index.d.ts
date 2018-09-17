363538273f44c6b5ecbcc2d35b4c41c184eb590b
Aaron Mc Adam <aaron@aaronmcadam.com>
2018-09-17 14:09:25 +0100

    first pass at copying dom-testing-library
    there's a few things I'm unsure of:
    
    * should we shadow the typings directory of dom-testing-library for easier maintainance?
    * allBy methods: will the list of items be typed correctly?

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,37 @@
-declare namespace Cypress {
-  interface Chainable<Subject = any> {
-    getByTestId<S = string>(x: string): Chainable<S>
+// TypeScript Version: 2.8
+import {
+  SelectorMatcherOptions,
+  Matcher,
+  MatcherOptions,
+  getByTestId,
+} from 'dom-testing-library'
+
+export namespace Cypress {
+  type GetByAttribute = (id: Matcher, options?: MatcherOptions) => Chainable
+
+  type QueryByText = (
+    id: Matcher,
+    options?: SelectorMatcherOptions,
+  ) => Chainable
+
+  type AllByText = (id: Matcher, options?: SelectorMatcherOptions) => Chainable
+
+  type GetByText = (id: Matcher, options?: SelectorMatcherOptions) => Chainable
+
+  interface Chainable {
+    getByPlaceholderText: GetByAttribute
+    queryByText: QueryByText
+    queryAllByText: AllByText
+    getByText: GetByText
+    getAllByText: AllByText
+    queryByLabelText: QueryByText
+    queryAllByLabelText: AllByText
+    getByLabelText: GetByText
+    getAllByLabelText: AllByText
+    getByAltText: GetByAttribute
+    getByTestId: GetByAttribute
+    getByTitle: GetByAttribute
+    getByValue: GetByAttribute
+    getByTRole: GetByAttribute
   }
 }
