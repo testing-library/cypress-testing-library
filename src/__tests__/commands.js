c40e7aeec0d31f1c5649adf77ea04c18ec59eadb
Kent C. Dodds <kent@doddsfamily.us>
2019-06-24 09:11:54 -0600

    test: remove flaky snapshot

--- src/__tests__/commands.js
+++ src/__tests__/commands.js
@@ -1,8 +1,11 @@
+import {queries} from '@testing-library/dom'
 import {commands} from '../'
 
 test('exports expected commands', () => {
   expect(commands).toMatchObject(expect.any(Array))
-  expect(commands.map(({name}) => name)).toMatchSnapshot()
+  const sortedQueryNames = Object.keys(queries).sort()
+  const sortedCommandNames = commands.map(({name}) => name).sort()
+  expect(sortedCommandNames).toEqual(sortedQueryNames)
   commands.forEach(command =>
     expect(command).toMatchObject({
       name: expect.any(String),
