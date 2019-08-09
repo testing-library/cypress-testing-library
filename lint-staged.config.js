5f282c74e531af335b126210f151d2f80e705ebe
Kent C. Dodds <kent@doddsfamily.us>
2019-08-09 14:39:15 -0600

    feat: upgrade all deps

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 const config = require('kcd-scripts/dist/config/lintstagedrc')
 
 // we need to exclude the test and lint scripts on commit because we run it in the validate script
-const jsLinter = Object.keys(config.linters).find(key => key.includes('js'))
-config.linters[jsLinter] = config.linters[jsLinter].filter(
+const jsLinter = Object.keys(config).find(key => key.includes('js'))
+config[jsLinter] = config[jsLinter].filter(
   script => !script.includes('test') && !script.includes('lint'),
 )
 
