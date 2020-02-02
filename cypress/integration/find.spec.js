testing-library/cypress-testing-library

commit e32c8e4d95dc7da88341b2886d4c9dc0bcea645e

feat: support running commands against the previous subject. (#100)
Also clean up similar within/container tests

src/add-commands.js
@@ -1,7 +1,7 @@
 import {commands} from './'
 
-commands.forEach(({name, command}) => {
-  Cypress.Commands.add(name, command)
+commands.forEach(({name, command, options = {}}) => {
+  Cypress.Commands.add(name, options, command)
 })
 
 /* global Cypress */

src/index.js
@@ -7,44 +7,50 @@ const getDefaultCommandOptions = () => {
   }
 }
 
-const queryNames = Object.keys(queries);
+const queryNames = Object.keys(queries)
 
-const getRegex = /^get/;
-const queryRegex = /^query/;
-const findRegex = /^find/;
+const getRegex = /^get/
+const queryRegex = /^query/
+const findRegex = /^find/
 
-const getQueryNames = queryNames.filter(q => getRegex.test(q));
-const queryQueryNames = queryNames.filter(q => queryRegex.test(q));
-const findQueryNames = queryNames.filter(q => findRegex.test(q));
+const getQueryNames = queryNames.filter(q => getRegex.test(q))
+const queryQueryNames = queryNames.filter(q => queryRegex.test(q))
+const findQueryNames = queryNames.filter(q => findRegex.test(q))
 
 const getCommands = getQueryNames.map(queryName => {
   return {
     name: queryName,
     command: () => {
       Cypress.log({
-        name: queryName
-      });
+        name: queryName,
+      })
 
-      throw new Error(`You used '${queryName}' which has been removed from Cypress Testing Library because it does not make sense in this context. Please use '${queryName.replace(getRegex, 'find')}' instead.`)
-    }
+      throw new Error(
+        `You used '${queryName}' which has been removed from Cypress Testing Library because it does not make sense in this context. Please use '${queryName.replace(
+          getRegex,
+          'find',
+        )}' instead.`,
+      )
+    },
   }
 })
 
 const queryCommands = queryQueryNames.map(queryName => {
-  return createCommand(queryName, queryName);
+  return createCommand(queryName, queryName)
 })
 
 const findCommands = findQueryNames.map(queryName => {
   // dom-testing-library find* queries use a promise to look for an element, but that doesn't work well with Cypress retryability
   // Use the query* commands so that we can lean on Cypress to do the retry for us
   // When it does return a null or empty array, Cypress will retry until the assertions are satisfied or the command times out
-  return createCommand(queryName, queryName.replace(findRegex, 'query'));
+  return createCommand(queryName, queryName.replace(findRegex, 'query'))
 })
 
 function createCommand(queryName, implementationName) {
   return {
     name: queryName,
-    command: (...args) => {
+    options: {prevSubject: ['optional', 'document', 'element', 'window']},
+    command: (prevSubject, ...args) => {
       const lastArg = args[args.length - 1]
       const defaults = getDefaultCommandOptions()
       const waitOptions =
@@ -52,41 +58,43 @@ function createCommand(queryName, implementationName) {
 
       const queryImpl = queries[implementationName]
       const baseCommandImpl = doc => {
-        const container = getContainer(waitOptions.container || doc)
+        const container = getContainer(
+          waitOptions.container || prevSubject || doc,
+        )
         return queryImpl(container, ...args)
       }
       const commandImpl = doc => baseCommandImpl(doc)
 
-      const inputArr = args.filter(filterInputs);
+      const inputArr = args.filter(filterInputs)
 
       const consoleProps = {
         // TODO: Would be good to completely separate out the types of input into their own properties
-        input: inputArr
+        input: inputArr,
       }
 
       Cypress.log({
         $el: inputArr,
         name: queryName,
         message: inputArr,
-        consoleProps: () => consoleProps
-      });
+        consoleProps: () => consoleProps,
+      })
 
       return cy
         .window({log: false})
-        .then({timeout: waitOptions.timeout + 100}, (thenArgs) => {
+        .then({timeout: waitOptions.timeout + 100}, thenArgs => {
           const getValue = () => {
-            const value = commandImpl(thenArgs.document);
-            const result = Cypress.$(value);
-          
+            const value = commandImpl(thenArgs.document)
+            const result = Cypress.$(value)
+
             // Overriding the selector of the jquery object because it's displayed in the long message of .should('exist') failure message
             // Hopefully it makes it clearer, because I find the normal response of "Expected to find element '', but never found it" confusing
-            result.selector = `${queryName}(${queryArgument(args)})`;
+            result.selector = `${queryName}(${queryArgument(args)})`
 
             if (result.length > 0) {
               consoleProps.yielded = result.toArray()
             }
 
-            return result;
+            return result
           }
 
           const resolveValue = () => {
@@ -100,19 +108,17 @@ function createCommand(queryName, implementationName) {
 
           if (queryRegex.test(queryName)) {
             // For get* queries, do not retry
-            return getValue();
+            return getValue()
           }
 
-          return resolveValue()
-            .then(subject => {
-    
-              // Remove the error that occurred because it is irrelevant now
-              if (consoleProps.error) {
-                delete consoleProps.error;
-              }
-    
-              return subject;
-            })
+          return resolveValue().then(subject => {
+            // Remove the error that occurred because it is irrelevant now
+            if (consoleProps.error) {
+              delete consoleProps.error
+            }
+
+            return subject
+          })
         })
     },
   }
@@ -125,33 +131,25 @@ function filterInputs(value) {
   if (value instanceof RegExp) {
     return value.toString()
   }
-  if (
-    typeof value === 'object' &&
-    Object.keys(value).length === 0
-  ) {
+  if (typeof value === 'object' && Object.keys(value).length === 0) {
     return false
   }
   return Boolean(value)
 }
 
 function queryArgument(args) {
-  const input = args
-    .find(value => {
-      return (value instanceof RegExp) || (typeof value === 'string')
-    });
+  const input = args.find(value => {
+    return value instanceof RegExp || typeof value === 'string'
+  })
 
-    if (input && typeof input === 'string') {
-      return `\`${input}\``;
-    }
+  if (input && typeof input === 'string') {
+    return `\`${input}\``
+  }
 
-    return input;
+  return input
 }
 
-const commands = [
-  ...getCommands,
-  ...findCommands,
-  ...queryCommands
-];
+const commands = [...getCommands, ...findCommands, ...queryCommands]
 
 export {commands, configure}
 

commit 547389d8605b699687546567914a4598da5a0797

feat: add more helpful debugging information to queries (#108)
* feat: add more helpful debugging information to queries
* Add element selector information for debugging (outlines element when you click on command) (fixes #103)
* Add @testing-library/dom errors (from `get*` queries) to failure messages - these are more helpful than the generic `find*('input') does not exist` messages (fixes #103)
* Add retryability to `findBy*` when multiple elements are found (fixes #83)
* Add option to disable logging of all commands
* `query*` and `find*` have a consistent code path and error messaging (fixes #103)
* Remove usage of Cypress commands in queries (fixes #103)

* feat: add ability for queries to inherit previous subject
* Fixes #109 without breaking change caused by #100

* feat: add parent/child log type detection

* chore: implement feedback

* docs: update readme to complete my thought process

* slightly simplify config fn

* Update README.md

Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

Closes #103, Closes #109, Closes #110

cypress/.eslintrc
@@ -1,0 +1,6 @@
+{
+  "rules": {
+    "max-lines-per-function": "off",
+    "jest/valid-expect-in-promise": "off"
+  }
+}

cypress/fixtures/test-app/index.html
@@ -29,6 +29,9 @@
 
     <label for="by-text-input-2">Label 2</label>
     <input type="text" placeholder="Input 2" id="by-text-input-2" />
+
+    <p>Intentionally inaccessible label for error checking</p>
+    <label>Label 3</label>
   </section>
   <section>
     <h2>*ByText</h2>
@@ -88,6 +91,24 @@
   <section>
     <h2>*ByText on another page</h2>
     <a onclick='setTimeout(function() { window.location = "/cypress/fixtures/test-app/next-page.html"; }, 100);'>Next Page</a>
+  </section>
+  <section>
+    <h2>Eventual existence</h2>
+    <button id="eventually-will-exist"></button>
+    <script>
+      setTimeout(() => {
+        document.querySelector('#eventually-will-exist').innerHTML = 'Eventually Exists'
+      }, 500)
+    </script>
+  </section>
+  <section>
+    <h2>Eventual non-existence</h2>
+    <button id="eventually-will-not-exist">Eventually not exists</button>
+    <script>
+      setTimeout(() => {
+        document.querySelector('#eventually-will-not-exist').remove()
+      }, 500)
+    </script>
   </section>
 <!-- Prettier unindents the script tag below -->
 <script>

src/add-commands.js
@@ -1,7 +1,11 @@
-import {commands} from './'
+import {configure, commands} from './'
 
-commands.forEach(({name, command}) => {
-  Cypress.Commands.add(name, command)
+commands.forEach(({name, command, options = {}}) => {
+  Cypress.Commands.add(name, options, command)
+})
+
+Cypress.Commands.add('configureCypressTestingLibrary', config => {
+  configure(config)
 })
 
 /* global Cypress */

src/index.js
@@ -1,10 +1,12 @@
-import {configure, queries} from '@testing-library/dom'
+import {configure as configureDTL, queries} from '@testing-library/dom'
 import {getContainer} from './utils'
 
-const getDefaultCommandOptions = () => {
-  return {
-    timeout: Cypress.config().defaultCommandTimeout,
+let globalFallbackRetryWithoutPreviousSubject = true
+function configure({fallbackRetryWithoutPreviousSubject, ...config}) {
+  if (fallbackRetryWithoutPreviousSubject != null) {
+    globalFallbackRetryWithoutPreviousSubject = fallbackRetryWithoutPreviousSubject
   }
+  return configureDTL(config)
 }
 
 const queryNames = Object.keys(queries)
@@ -32,87 +34,153 @@ const getCommands = getQueryNames.map(queryName => {
 })
 
 const queryCommands = queryQueryNames.map(queryName => {
-  return createCommand(queryName, queryName)
+  return createCommand(queryName, queryName.replace(queryRegex, 'get'))
 })
 
 const findCommands = findQueryNames.map(queryName => {
-  // dom-testing-library find* queries use a promise to look for an element, but that doesn't work well with Cypress retryability
-  // Use the query* commands so that we can lean on Cypress to do the retry for us
-  // When it does return a null or empty array, Cypress will retry until the assertions are satisfied or the command times out
-  return createCommand(queryName, queryName.replace(findRegex, 'query'))
+  return createCommand(queryName, queryName.replace(findRegex, 'get'))
 })
 
 function createCommand(queryName, implementationName) {
   return {
     name: queryName,
-    command: (...args) => {
+    options: {prevSubject: ['optional', 'document', 'element', 'window']},
+    command: (prevSubject, ...args) => {
       const lastArg = args[args.length - 1]
-      const defaults = getDefaultCommandOptions()
-      const waitOptions =
+      const defaults = {
+        // make the timeout extremely short to ensure `query*` commands pass or fail instantly
+        timeout: queryRegex.test(queryName) ? 0 : Cypress.config().defaultCommandTimeout,
+        // setting this to false will disable the fallback to querying without a previous subject
+        // This is to prevent breaking changes, but also allow for prevSubject scoping
+        fallbackRetryWithoutPreviousSubject: globalFallbackRetryWithoutPreviousSubject,
+        log: true,
+      }
+      const options =
         typeof lastArg === 'object' ? {...defaults, ...lastArg} : defaults
 
       const queryImpl = queries[implementationName]
-      const baseCommandImpl = doc => {
-        const container = getContainer(waitOptions.container || doc)
-        return queryImpl(container, ...args)
+      const baseCommandImpl = container => {
+        return queryImpl(getContainer(container), ...args)
       }
-      const commandImpl = doc => baseCommandImpl(doc)
+      const commandImpl = container => baseCommandImpl(container)
 
       const inputArr = args.filter(filterInputs)
+
+      const getSelector = () => `${queryName}(${queryArgument(args)})`
+
+      const win = cy.state('window')
 
       const consoleProps = {
         // TODO: Would be good to completely separate out the types of input into their own properties
         input: inputArr,
+        Selector: getSelector(),
+        'Applied To': getContainer(
+          options.container || prevSubject || win.document,
+        )
       }
 
-      Cypress.log({
-        $el: inputArr,
-        name: queryName,
-        message: inputArr,
-        consoleProps: () => consoleProps,
-      })
-
-      return cy
-        .window({log: false})
-        .then({timeout: waitOptions.timeout + 100}, thenArgs => {
-          const getValue = () => {
-            const value = commandImpl(thenArgs.document)
-            const result = Cypress.$(value)
-
-            // Overriding the selector of the jquery object because it's displayed in the long message of .should('exist') failure message
-            // Hopefully it makes it clearer, because I find the normal response of "Expected to find element '', but never found it" confusing
-            result.selector = `${queryName}(${queryArgument(args)})`
-
-            if (result.length > 0) {
-              consoleProps.yielded = result.toArray()
-            }
-
-            return result
-          }
-
-          const resolveValue = () => {
-            // retry calling "getValue" until following assertions pass or this command times out
-            return Cypress.Promise.try(getValue).then(value => {
-              return cy.verifyUpcomingAssertions(value, waitOptions, {
-                onRetry: resolveValue,
-              })
-            })
-          }
-
-          if (queryRegex.test(queryName)) {
-            // For get* queries, do not retry
-            return getValue()
-          }
-
-          return resolveValue().then(subject => {
-            // Remove the error that occurred because it is irrelevant now
-            if (consoleProps.error) {
-              delete consoleProps.error
-            }
-
-            return subject
-          })
+      if (options.log) {
+        options._log = Cypress.log({
+          type: prevSubject ? 'child' : 'parent',
+          name: queryName,
+          message: inputArr,
+          consoleProps: () => consoleProps,
         })
+      }
+
+      const getValue = (container = options.container || prevSubject || win.document) => {
+        const value = commandImpl(container)
+
+        const result = Cypress.$(value)
+        if (value && options._log) {
+          options._log.set('$el', result)
+        }
+
+        // Overriding the selector of the jquery object because it's displayed in the long message of .should('exist') failure message
+        // Hopefully it makes it clearer, because I find the normal response of "Expected to find element '', but never found it" confusing
+        result.selector = getSelector()
+
+        consoleProps.elements = result.length
+        if (result.length === 1) {
+          consoleProps.yielded = result.toArray()[0]
+        } else if (result.length > 0) {
+          consoleProps.yielded = result.toArray()
+        }
+
+        if (result.length > 1 && !/All/.test(queryName)) {
+          // Is this useful?
+          throw Error(`Found multiple elements with the text: ${queryArgument(args)}`)
+        }
+
+        return result
+      }
+
+      // This state tracking is not ideal, but it allows detection of compatibility mode for a warning message
+      let failedNewFunctionality = false
+      let failedOldFunctionality = false
+
+      let error
+
+      // Errors will be thrown by @testing-library/dom, but a query might be followed by `.should('not.exist')`
+      // We just need to capture the error thrown by @testing-library/dom and return an empty jQuery NodeList
+      // to allow Cypress assertions errors to happen naturally. If an assertion fails, we'll have a helpful
+      // error message handy to pass on to the user
+      const catchQueryError = err => {
+        error = err
+        failedOldFunctionality = true
+        const result = Cypress.$()
+        result.selector = getSelector()
+        return result
+      }
+
+      // Before https://github.com/testing-library/cypress-testing-library/pull/100,
+      // queries were run without being scoped to previous subjects. There is code now that depends
+      // on functionality before #100. See if we can succeed using old functionality before finally failing
+      // This function can be removed as a breaking change
+      const catchAndTryOldFunctionality = err => {
+        error = err
+        failedNewFunctionality = true
+        const container = options.fallbackRetryWithoutPreviousSubject ? options.container || win.document : undefined
+        return getValue(container)
+      }
+
+      const resolveValue = () => {
+        // retry calling "getValue" until following assertions pass or this command times out
+        return Cypress.Promise.try(getValue)
+          .catch(catchAndTryOldFunctionality)
+          .catch(catchQueryError)
+          .then(value => {
+            return cy.verifyUpcomingAssertions(value, options, {
+              onRetry: resolveValue,
+              onFail: () => {
+                // We want to override Cypress's normal non-existence message with @testing-library/dom's more helpful ones
+                if (error) {
+                  options.error = error
+                }
+              }
+            })
+        })
+      }
+
+      return resolveValue().then(subject => {
+        // Remove the error that occurred because it is irrelevant now
+        if (consoleProps.error) {
+          delete consoleProps.error
+        }
+        if (options._log) {
+          options._log.snapshot()
+        }
+
+        return subject
+      }).finally(() => {
+        if (options._log) {
+          if (failedNewFunctionality && !failedOldFunctionality) {
+            options._log.error(Error(`@testing-library/cypress will eventually only use previous subjects when queries are added to a chain of commands. We've detected an instance where the this functionality failed, but the old functionality passed (so your test may break in a future version). Please use cy.${queryName}(${queryArgument(args)}) instead of continuing from a previous chain.`))
+          } else {
+            options._log.end()
+          }
+        }
+      })
     },
   }
 }

cypress/integration/configure.spec.js
@@ -1,0 +1,18 @@
+/// <reference types="cypress" />
+describe('configuring fallback globally', () => {
+  beforeEach(() => {
+    cy.visit('cypress/fixtures/test-app/')
+    cy.configureCypressTestingLibrary({ fallbackRetryWithoutPreviousSubject: false })
+  })
+
+  it('findByText with a previous subject', () => {
+    cy.get('#nested')
+      .findByText('Button Text 1')
+      .should('not.exist')
+    cy.get('#nested')
+      .findByText('Button Text 2')
+      .should('exist')
+  })
+})
+
+/* global cy */

cypress/integration/find.spec.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 describe('find* dom-testing-library commands', () => {
   beforeEach(() => {
     cy.visit('cypress/fixtures/test-app/')
@@ -86,6 +87,21 @@ describe('find* dom-testing-library commands', () => {
 
   /* Test the behaviour around these queries */
 
+  it('findByText should handle non-existence', () => {
+    cy.findByText('Does Not Exist')
+      .should('not.exist')
+  })
+
+  it('findByText should handle eventual existence', () => {
+    cy.findByText('Eventually Exists')
+      .should('exist')
+  })
+
+  it('findByText should handle eventual non-existence', () => {
+    cy.findByText('Eventually Not exists')
+      .should('not.exist')
+  })
+
   it("findByText with should('not.exist')", () => {
     cy.findAllByText(/^Button Text \d$/).should('exist')
     cy.findByText('Non-existing Button Text', {timeout: 100}).should(
@@ -93,15 +109,26 @@ describe('find* dom-testing-library commands', () => {
     )
   })
 
+  it('findByText with a previous subject', () => {
+    cy.get('#nested')
+      .findByText('Button Text 1', { fallbackRetryWithoutPreviousSubject: false })
+      .should('not.exist')
+    cy.get('#nested')
+      .findByText('Button Text 2')
+      .should('exist')
+  })
+
   it('findByText within', () => {
     cy.get('#nested').within(() => {
-      cy.findByText('Button Text 2').click()
+      cy.findByText('Button Text 1').should('not.exist')
+      cy.findByText('Button Text 2').should('exist')
     })
   })
 
   it('findByText in container', () => {
-    return cy.get('#nested').then(subject => {
-      cy.findByText(/^Button Text/, {container: subject}).click()
+    cy.get('#nested').then(subject => {
+      cy.findByText('Button Text 1', {container: subject}).should('not.exist')
+      cy.findByText('Button Text 2', {container: subject}).should('exist')
     })
   })
 
@@ -110,23 +137,87 @@ describe('find* dom-testing-library commands', () => {
     cy.findByText('New Page Loaded').should('exist')
   })
 
-  it('findByText should error if no elements are found', () => {
-    const regex = /Supercalifragilistic/
-    const errorMessage = `Timed out retrying: Expected to find element: 'findByText(${regex})', but never found it.`
-    cy.on('fail', err => {
-      expect(err.message).to.eq(errorMessage)
+  it('findByText should set the Cypress element to the found element', () => {
+    // This test is a little strange since snapshots show what element
+    // is selected, but snapshots themselves don't give access to those
+    // elements. I had to make the implementation specific so that the `$el`
+    // is the `subject` when the log is added and the `$el` is the `value`
+    // when the log is changed. It would be better to extract the `$el` from
+    // each snapshot
+
+    cy.on('log:changed', (attrs, log) => {
+      if (log.get('name') === 'findByText') {
+        expect(log.get('$el')).to.have.text('Button Text 1')
+      }
     })
 
-    cy.findByText(regex, {timeout: 100}) // Doesn't explicitly need .should('exist') if it's the last element?
+    cy.findByText('Button Text 1')
+  })
+
+  it('findByText should error if no elements are found', () => {
+    const regex = /Supercalifragilistic/
+    const errorMessage = `Unable to find an element with the text: /Supercalifragilistic/`
+    cy.on('fail', err => {
+      expect(err.message).to.contain(errorMessage)
+    })
+
+    cy.findByText(regex, {timeout: 100})
+  })
+
+  it('findByText should default to Cypress non-existence error message', () => {
+    const errorMessage = `Expected <button> not to exist in the DOM, but it was continuously found.`
+    cy.on('fail', err => {
+      expect(err.message).to.contain(errorMessage)
+    })
+
+    cy.findByText('Button Text 1', {timeout: 100})
+      .should('not.exist')
+  })
+
+  it('findByLabelText should forward useful error messages from @testing-library/dom', () => {
+    const errorMessage = `Found a label with the text of: Label 3, however no form control was found associated to that label.`
+    cy.on('fail', err => {
+      expect(err.message).to.contain(errorMessage)
+    })
+
+    cy.findByLabelText('Label 3', {timeout: 100})
   })
 
   it('findByText finding multiple items should error', () => {
     const errorMessage = `Found multiple elements with the text: /^Button Text/i\n\n(If this is intentional, then use the \`*AllBy*\` variant of the query (like \`queryAllByText\`, \`getAllByText\`, or \`findAllByText\`)).`
     cy.on('fail', err => {
-      expect(err.message).to.eq(errorMessage)
+      expect(err.message).to.contain(errorMessage)
     })
 
-    cy.findByText(/^Button Text/i)
+    cy.findByText(/^Button Text/i, {timeout: 100})
+  })
+
+  it('findByText should not break existing code', () => {
+    cy.window()
+      .findByText('Button Text 1')
+      .should('exist')
+  })
+
+  it('findByText should show as a parent command if it starts a chain', () => {
+    const assertLog = (attrs, log) => {
+      if(log.get('name') === 'findByText') {
+        expect(log.get('type')).to.equal('parent')
+        cy.off('log:added', assertLog)
+      }
+    }
+    cy.on('log:added', assertLog)
+    cy.findByText('Button Text 1')
+  })
+
+  it('findByText should show as a child command if it continues a chain', () => {
+    const assertLog = (attrs, log) => {
+      if(log.get('name') === 'findByText') {
+        expect(log.get('type')).to.equal('child')
+        cy.off('log:added', assertLog)
+      }
+    }
+    cy.on('log:added', assertLog)
+    cy.get('body').findByText('Button Text 1')
   })
 })
 

cypress/integration/query.spec.js
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 describe('query* dom-testing-library commands', () => {
   beforeEach(() => {
     cy.visit('cypress/fixtures/test-app/')
@@ -95,12 +96,30 @@ describe('query* dom-testing-library commands', () => {
     })
   })
 
+  it('queryByText should set the Cypress element to the found element', (done) => {
+    // This test is a little strange since snapshots show what element
+    // is selected, but snapshots themselves don't give access to those
+    // elements. I had to make the implementation specific so that the `$el`
+    // is the `subject` when the log is added and the `$el` is the `value`
+    // when the log is changed. It would be better to extract the `$el` from
+    // each snapshot
+
+    cy.on('log:changed', (attrs, log) => {
+      if (log.get('name') === 'queryByText') {
+        expect(log.get('$el')).to.have.text('Button Text 1')
+        done()
+      }
+    })
+
+    cy.queryByText('Button Text 1')
+  })
+
   it('query* will return immediately, and never retry', () => {
     cy.queryByText('Next Page').click()
 
-    const errorMessage = `expected 'queryByText(\`New Page Loaded\`)' to exist in the DOM`
+    const errorMessage = `Unable to find an element with the text: New Page Loaded.`
     cy.on('fail', err => {
-      expect(err.message).to.eq(errorMessage)
+      expect(err.message).to.contain(errorMessage)
     })
 
     cy.queryByText('New Page Loaded', { timeout: 300 }).should('exist')
@@ -129,23 +148,42 @@ describe('query* dom-testing-library commands', () => {
       .and('not.exist')
   })
 
-  it('queryAllByText with a should(\'exist\') must provide selector error message', () => {
+  it('queryAllByText should forward existence error message from @testing-library/dom', () => {
     const text = 'Supercalifragilistic'
-    const errorMessage = `expected 'queryAllByText(\`${text}\`)' to exist in the DOM`
+    const errorMessage = `Unable to find an element with the text: Supercalifragilistic.`
     cy.on('fail', err => {
-      expect(err.message).to.eq(errorMessage)
+      expect(err.message).to.contain(errorMessage)
     })
 
-    cy.queryAllByText(text, {timeout: 100}).should('exist') // NOT POSSIBLE WITH QUERYALL?
+    cy.queryAllByText(text, {timeout: 100}).should('exist')
+  })
+
+  it('queryByLabelText should forward useful error messages from @testing-library/dom', () => {
+    const errorMessage = `Found a label with the text of: Label 3, however no form control was found associated to that label.`
+    cy.on('fail', err => {
+      expect(err.message).to.contain(errorMessage)
+    })
+
+    cy.queryByLabelText('Label 3', {timeout: 100}).should('exist')
+  })
+
+  it('queryAllByText should default to Cypress non-existence error message', () => {
+    const errorMessage = `Expected <button> not to exist in the DOM, but it was continuously found.`
+    cy.on('fail', err => {
+      expect(err.message).to.contain(errorMessage)
+    })
+
+    cy.queryAllByText('Button Text 1', {timeout: 100})
+      .should('not.exist')
   })
 
   it('queryByText finding multiple items should error', () => {
-    const errorMessage = `Found multiple elements with the text: /^queryByText/i\n\n(If this is intentional, then use the \`*AllBy*\` variant of the query (like \`queryAllByText\`, \`getAllByText\`, or \`findAllByText\`)).`
+    const errorMessage = `Found multiple elements with the text: /^Button Text/i\n\n(If this is intentional, then use the \`*AllBy*\` variant of the query (like \`queryAllByText\`, \`getAllByText\`, or \`findAllByText\`)).`
     cy.on('fail', err => {
-      expect(err.message).to.eq(errorMessage)
+      expect(err.message).to.contain(errorMessage)
     })
 
-    cy.queryByText(/^queryByText/i)
+    cy.queryByText(/^Button Text/i)
   })
 })
 

src/__tests__/add-commands.js
@@ -7,10 +7,11 @@ test('adds commands to Cypress', () => {
 
   require('../add-commands')
 
-  expect(addMock).toHaveBeenCalledTimes(commands.length)
+  expect(addMock).toHaveBeenCalledTimes(commands.length + 1) // we're also adding a configuration command
   commands.forEach(({name}, index) => {
     expect(addMock.mock.calls[index]).toMatchObject([
       name,
+      {},
       // We get a new function that is `command.bind(null, cy)` i.e. global `cy` passed into the first argument.
       // The commands themselves will be tested separately in the Cypress end-to-end tests.
       expect.any(Function),