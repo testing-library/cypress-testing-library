fdaa7e54a104e071924560040042bdb79f9ef283
Joseph Psychas <3214876+TheGallery@users.noreply.github.com>
2020-08-22 23:40:54 +0100

    feat(TS): move types from DefinitelyTyped (#148)
    Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

---
+++ types/test.ts
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+import { configure } from '@testing-library/cypress'
+import '@testing-library/cypress/add-commands'
+
+configure({ testIdAttribute: 'data-myown-testid' })
+
+// findBy*
+cy.findByPlaceholderText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByLabelText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByAltText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByTestId('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByTitle('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByDisplayValue('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findByRole('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+
+// findAllBy*
+cy.findAllByPlaceholderText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByLabelText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByAltText('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByTestId('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByTitle('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByDisplayValue('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
+cy.findAllByRole('foo') // $ExpectType Chainable<JQuery<HTMLElement>>
