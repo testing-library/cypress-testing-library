0b62eb85dd1c7c8de6bef225d3ac031d26cc4aec
Mohsen Azimi <mohsen.azimi@airbnb.com>
2023-05-25 12:08:16 +0200

    Use Cypress.Commands.overwriteQuery when adding a command that already exist

--- src/__tests__/add-commands.js
+++ src/__tests__/add-commands.js
@@ -3,15 +3,41 @@ import {commands} from '../'
 test('adds commands to Cypress', () => {
   const addMock = jest.fn().mockName('Cypress.Commands.add')
   const addQueryMock = jest.fn().mockName('Cypress.Commands.addQuery')
-  global.Cypress = {Commands: {add: addMock, addQuery: addQueryMock}}
-  global.cy = {}
+  const overwriteQueryMock = jest
+    .fn()
+    .mockName('Cypress.Commands.overwriteQuery')
+  global.Cypress = {
+    Commands: {
+      add: addMock,
+      addQuery: addQueryMock,
+      overwriteQuery: overwriteQueryMock,
+    },
+  }
+  global.cy = {
+    findAllByLabelText: jest.fn(),
+  }
 
   require('../add-commands')
 
-  expect(addQueryMock).toHaveBeenCalledTimes(commands.length)
+  /** Commands that we're adding to the cy. namespace */
+  const newCommands = commands.filter(({name}) => !global.cy[name])
+  /** Commands that we're overwriting in the cy. namespace */
+  const overwrittenCommands = commands.filter(({name}) => global.cy[name])
+
+  expect(addQueryMock).toHaveBeenCalledTimes(newCommands.length)
+  expect(overwriteQueryMock).toHaveBeenCalledTimes(overwrittenCommands.length)
   expect(addMock).toHaveBeenCalledTimes(1) // we're also adding a configuration command
-  commands.forEach(({name}, index) => {
+
+  newCommands.forEach(({name}, index) => {
     expect(addQueryMock.mock.calls[index]).toMatchObject([
+      name,
+      // We get a new function that is `command.bind(null, cy)` i.e. global `cy` passed into the first argument.
+      // The commands themselves will be tested separately in the Cypress end-to-end tests.
+      expect.any(Function),
+    ])
+  })
+  overwrittenCommands.forEach(({name}, index) => {
+    expect(overwriteQueryMock.mock.calls[index]).toMatchObject([
       name,
       // We get a new function that is `command.bind(null, cy)` i.e. global `cy` passed into the first argument.
       // The commands themselves will be tested separately in the Cypress end-to-end tests.
